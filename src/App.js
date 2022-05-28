5b9f165fb23c3b8232cdf3193a5a45df5a6bbd0f
yumihong <ghddid13@naver.com>
2022-05-28 12:19:11 +0900

    trello 카드 작성중

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ function App() {
       <nav className="navbar app"> App bar</nav>
       <nav className="navbar board"> Board bar</nav>
       <div className="board-columns">
-        board columns
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120c/download/Design.png" />
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120c/download/Design.png" />
+              Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
       </div>
     </div>
   );
